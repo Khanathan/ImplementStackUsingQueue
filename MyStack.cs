2eb16fe9080481459fa0029196efc587290f8d97
Khanathan <dgk25500@gmail.com>
2022-10-11 15:27:29 -0700

    Add project files.

---
+++ MyStack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplementStackUsingQueue
+{
+    public class MyStack
+    {
+        //Constructor: Empty stack starts with Count = 0;
+        public MyStack()
+        {
+            count = 0;
+        }
+
+        private Queue<int> q1 = new Queue<int>();
+        private Queue<int> q2 = new Queue<int>();
+
+
+        public void Push(int input)
+        {
+            //Enqueue pushed item to q2.
+            q2.Enqueue(input);
+
+            //Transfer all q1 items to q2, newly pushed items stay at the beginning of the queue/Stack.
+            //q1 will be empty at the end of the while loop.
+            while (q1.Count > 0)
+            {
+                q2.Enqueue(q1.Dequeue());
+            }
+
+            //Swap names of q1 and q2, making q1 the Queue containing the Stack items.
+            Queue<int> qTemp = q1;
+            q1 = q2;
+            q2 = qTemp;
+            count++;
+        }
+
+        public int Pop()
+        {
+            count--;
+            return q1.Dequeue();            
+        }
+
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ImplementStackUsingQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack testStack = new MyStack();
+            int? inputInt = 0;
+
+            //Welcome message that prompts the user for input
+            Console.WriteLine("Welcome to my stack. Please enter a real number to add to the Stack.");
+
+            //Loop repeats until an empty input is entered.
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "") { break; }
+                //Check for invalid input (only works for real numbers)
+                while (!input.All(Char.IsDigit))
+                {
+                    Console.WriteLine("You have entered an invalid real number, please try again.");
+                    input = Console.ReadLine();
+                }
+
+                inputInt = Convert.ToInt32(input);                
+                
+                testStack.Push((int)inputInt);
+                Console.WriteLine("Enter another number to add to the stack or hit enter with no input to stop adding.");
+            }
+       
+            Console.WriteLine("The Stack currently has " + testStack.Count + " items.");
+
+            while (testStack.Count != 0)
+            {
+                Console.WriteLine(testStack.Pop());
+            }
+            Console.WriteLine("Done.");
+            Console.ReadLine();
+            
+        }
+    }
+}
